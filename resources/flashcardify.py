0fbad7ca1340cc51219e3f53dcb94bb0dc274307
joeljosephreji <joeljosephreji@gmail.com>
2024-03-03 13:02:51 +1100

    putting in the code which needs to be replaced by the actual transformers

--- resources/flashcardify.py
+++ resources/flashcardify.py
@@ -1,4 +1,10 @@
 from flask_restful import Resource, reqparse
+import transformers
+
+
+def extract_flashcards_from_transformer(notes):
+    print(notes)
+    return notes
 
 class Flashcardify(Resource):
     def post(self):
@@ -9,6 +15,6 @@ class Flashcardify(Resource):
         notes = args[name_of_notes]
 
         returned_response = {}
-        returned_response['flashcards'] = notes
+        returned_response['flashcards'] = extract_flashcards_from_transformer(notes)
         return returned_response, 200
 
